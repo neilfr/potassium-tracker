b3c370c13aff54c8cedf83e625f4ecc638844dae
Neil Froggatt <neilfr@live.com>
2021-10-19 09:24:41 -0400

    set new conversionfactors as favourites

--- app/Http/Controllers/Conversionfactor/StoreController.php
+++ app/Http/Controllers/Conversionfactor/StoreController.php
@@ -7,6 +7,7 @@ use App\Models\Conversionfactor;
 use App\Models\Foodname;
 use App\Models\Measurename;
 use App\Models\Nutrientname;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
@@ -28,11 +29,13 @@ class StoreController extends Controller
         $measurename->MeasureDescription = $request->measureDescription;
         $measurename->save();
 
+        $user = User::find(auth()->user()->id);
+
         $conversionfactor = new Conversionfactor;
         $conversionfactor->FoodID = $foodname->FoodID;
         $conversionfactor->MeasureID = $measurename->MeasureID;
         $conversionfactor->ConversionFactorValue = 1;
-        $conversionfactor->user_id = auth()->user()->id;
+        $conversionfactor->user_id = $user->id;
         $conversionfactor->save();
 
         $conversionfactor->foodname->nutrientnames()->attach(306,[
@@ -41,6 +44,13 @@ class StoreController extends Controller
         $conversionfactor->foodname->nutrientnames()->attach(208,[
             'NutrientValue' => $request->kcal,
         ]);
+
+        $freshConversionfactor = Conversionfactor::query()
+            ->where('FoodID', $foodname->FoodID)
+            ->where('MeasureID', $measurename->MeasureID)
+            ->first();
+
+        $user->favourites()->attach($freshConversionfactor);
 
         return redirect()->route('conversionfactors.index');
     }
