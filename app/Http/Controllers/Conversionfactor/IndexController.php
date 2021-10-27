5029c9ff6b166f39344d27078064c0cb64fefc03
Neil Froggatt <neilfr@live.com>
2021-10-26 21:30:11 -0400

    cleanup old unused files

--- app/Http/Controllers/Conversionfactor/IndexController.php
+++ app/Http/Controllers/Conversionfactor/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function __invoke(Request $request)
     {
